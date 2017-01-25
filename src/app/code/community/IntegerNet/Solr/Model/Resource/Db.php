1e76a688e9991396e21a1b555d1fd43f56aace8a
Fabian Schmengler <fs@integer-net.de>
2017-01-25 17:47:04 +0100

    Do not close MySQL connections with open transactions
    Transactions are used if products are saved. To update the index of a
    single product, disconnecting from the database does not make sense
    anyway.
    
    Resolves #6

--- src/app/code/community/IntegerNet/Solr/Model/Resource/Db.php
+++ src/app/code/community/IntegerNet/Solr/Model/Resource/Db.php
@@ -19,10 +19,24 @@ class IntegerNet_Solr_Model_Resource_Db
         /** @var Zend_Db_Adapter_Abstract $connection */
         foreach (Mage::getSingleton('core/resource')->getConnections() as $name => $connection) {
             if ($connection instanceof Zend_Db_Adapter_Abstract) {
+                if ($this->isTransactionOpen($connection)) {
+                    continue;
+                }
                 $connection->closeConnection();
             }
         }
         // connections (adapter objects) must be fully reinitialized, otherwise initStatements are not executed
         Mage::unregister('_singleton/core/resource');
+    }
+
+    /**
+     * Returns true if the given connection has open transactions
+     *
+     * @param Zend_Db_Adapter_Abstract $connection
+     * @return bool
+     */
+    private function isTransactionOpen(Zend_Db_Adapter_Abstract $connection)
+    {
+        return $connection instanceof Magento_Db_Adapter_Pdo_Mysql && $connection->isTransaction();
     }
 }

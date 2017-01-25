7e4e3da82ceef1e2989ea507c8b1c7ac12af3164
Fabian Schmengler <fs@integer-net.de>
2017-01-25 18:14:10 +0100

    Test if index update on save actually works

--- src/app/code/community/IntegerNet/Solr/Test/Model/Indexer/Product.php
+++ src/app/code/community/IntegerNet/Solr/Test/Model/Indexer/Product.php
@@ -47,5 +47,29 @@ class IntegerNet_Solr_Test_Model_Indexer_Product extends EcomDev_PHPUnit_Test_Ca
         $product = Mage::getModel('catalog/product')->load($productId);
         $product->setData('name', 'Product One SUPERDUPER');
         $product->save();
+        $searchResponse = $this->searchFor('SUPERDUPER');
+        $this->assertCount(1, $searchResponse->documents());
+    }
+
+    /**
+     * @param $queryText
+     * @return \IntegerNet\Solr\Resource\SolrResponse
+     */
+    public function searchFor($queryText)
+    {
+        $storeId = 1;
+        $queryStub = $this->getMockBuilder(\IntegerNet\Solr\Implementor\HasUserQuery::class)
+            ->getMockForAbstractClass();
+        $queryStub->method('getUserQueryText')->willReturn($queryText);
+        $factory = Mage::helper('integernet_solr/factory');
+        $applicationContext = $factory->getApplicationContext();
+        $applicationContext->setFuzzyConfig($factory->getCurrentStoreConfig()->getFuzzySearchConfig());
+        $applicationContext->setQuery($queryStub);
+        $searchRequestFactory = new \IntegerNet\Solr\Request\SearchRequestFactory(
+            $applicationContext,
+            $factory->getSolrResource(),
+            $storeId
+        );
+        return $searchRequestFactory->createRequest()->doRequest();
     }
 }
